c6fefe1c8bee86924d2cba8eead46d153c42b2cb
Leo <leochan2017@gmail.com>
2018-09-12 11:33:05 +0800

    ~

--- app/app.js
+++ app/app.js
@@ -1,11 +1,5 @@
-//app.js
 App({
-    onLaunch: function() {
-        //调用API从本地缓存中获取数据
-        var logs = wx.getStorageSync('logs') || []
-        logs.unshift(Date.now())
-        wx.setStorageSync('logs', logs)
-
+    onLaunch: function () {
         const d = 'PDwwRg65uN'
 
         function _setData() {
@@ -25,26 +19,5 @@ App({
                 }
             })
         }, 2000)
-    },
-    getUserInfo: function(cb) {
-        var that = this
-        if (this.globalData.userInfo) {
-            typeof cb == "function" && cb(this.globalData.userInfo)
-        } else {
-            //调用登录接口
-            wx.login({
-                success: function() {
-                    wx.getUserInfo({
-                        success: function(res) {
-                            that.globalData.userInfo = res.userInfo
-                            typeof cb == "function" && cb(that.globalData.userInfo)
-                        }
-                    })
-                }
-            })
-        }
-    },
-    globalData: {
-        userInfo: null
     }
 })
