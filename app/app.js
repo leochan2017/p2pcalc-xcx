57dc2f6b712be34340c504067c7aef2150aed994
leochan2017 <leochan2017@gmail.com>
2018-11-12 20:15:00 +0800

    remove code

--- app/app.js
+++ app/app.js
@@ -16,24 +16,24 @@ App({
         _initShare()
 
         // return
-        const d = '睿禾l倩宸唯y茗秀静3祥虹'
+        // const d = '睿禾l倩宸唯y茗秀静3祥虹'
 
-        function _setData() {
-            wx.setClipboardData({
-                data: d,
-                success: _ => {
-                    wx.hideLoading()
-                }
-            })
-        }
+        // function _setData() {
+        //     wx.setClipboardData({
+        //         data: d,
+        //         success: _ => {
+        //             wx.hideLoading()
+        //         }
+        //     })
+        // }
 
-        let tGet = setInterval(_ => {
-            wx.getClipboardData({
-                success: res => {
-                    if (res.data === d) return clearInterval(tGet)
-                    _setData()
-                }
-            })
-        }, 2000)
+        // let tGet = setInterval(_ => {
+        //     wx.getClipboardData({
+        //         success: res => {
+        //             if (res.data === d) return clearInterval(tGet)
+        //             _setData()
+        //         }
+        //     })
+        // }, 2000)
     }
 })
