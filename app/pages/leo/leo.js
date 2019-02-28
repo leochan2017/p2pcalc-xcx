8800bff05b31806ed05c8dd823ff766df54ff5a4
leochan <leochan2017@gmail.com>
2019-02-28 22:48:17 +0800

    去掉“更多程序”

--- app/pages/leo/leo.js
+++ app/pages/leo/leo.js
@@ -16,55 +16,55 @@ Page({
                 })
             },
             fail: err => {
-                wx.previewImage({ urls: ['http://www.leojs.com/pay-me-money.jpg'] })
+                wx.previewImage({ urls: ['https://github.com/leochan2017/zgjm/blob/master/%E7%B4%A0%E6%9D%90/pay-me-money.jpg?raw=true'] })
             }
         })
     },
     onLoad() {
         wx.setNavigationBarTitle({ title: '关于' })
     },
-    payMeBtc() {
-        wx.setClipboardData({
-            data: '1PQg7wJNMVa3wZxtrMJiFmcHrQicsDtpsG',
-            success: res => {
-                wx.showModal({
-                    title: '感谢您的支持!',
-                    content: 'BTC钱包收款地址已经复制至您的手机剪切板',
-                    showCancel: false,
-                    confirmText: '好的'
-                })
+    // payMeBtc() {
+    //     wx.setClipboardData({
+    //         data: '1PQg7wJNMVa3wZxtrMJiFmcHrQicsDtpsG',
+    //         success: res => {
+    //             wx.showModal({
+    //                 title: '感谢您的支持!',
+    //                 content: 'BTC钱包收款地址已经复制至您的手机剪切板',
+    //                 showCancel: false,
+    //                 confirmText: '好的'
+    //             })
 
-                wx.setStorage({
-                    key: 'isPayLeoMoney',
-                    data: true
-                })
-            },
-            fail: err => {
-                wx.showToast({ title: '复制失败，请重试' })
-            }
-        })
-    },
-    payMeEth() {
-        wx.setClipboardData({
-            data: '0xd7EAe8051D4309686c27D96cdE3C41A0ba91c43f',
-            success: res => {
-                wx.showModal({
-                    title: '感谢您的支持!',
-                    content: 'imtoken钱包收款地址已经复制至您的手机剪切板',
-                    showCancel: false,
-                    confirmText: '好的'
-                })
-                
-                wx.setStorage({
-                    key: 'isPayLeoMoney',
-                    data: true
-                })
-            },
-            fail: err => {
-                wx.showToast({ title: '复制失败，请重试' })
-            }
-        })
-    },
+    //             wx.setStorage({
+    //                 key: 'isPayLeoMoney',
+    //                 data: true
+    //             })
+    //         },
+    //         fail: err => {
+    //             wx.showToast({ title: '复制失败，请重试' })
+    //         }
+    //     })
+    // },
+    // payMeEth() {
+    //     wx.setClipboardData({
+    //         data: '0xd7EAe8051D4309686c27D96cdE3C41A0ba91c43f',
+    //         success: res => {
+    //             wx.showModal({
+    //                 title: '感谢您的支持!',
+    //                 content: 'imtoken钱包收款地址已经复制至您的手机剪切板',
+    //                 showCancel: false,
+    //                 confirmText: '好的'
+    //             })
+
+    //             wx.setStorage({
+    //                 key: 'isPayLeoMoney',
+    //                 data: true
+    //             })
+    //         },
+    //         fail: err => {
+    //             wx.showToast({ title: '复制失败，请重试' })
+    //         }
+    //     })
+    // },
     onShareBtnClick() {
         let share = wx.getStorageSync('share') || 1
         share = share + 1
