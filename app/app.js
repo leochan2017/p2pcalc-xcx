59f0a30993d81d9bd42e68e463418821fca83ee9
Leo <leochan2017@gmail.com>
2019-04-24 11:15:56 +0800

    1、增加激励广告；2，交由后台控制广告是否显示

--- app/app.js
+++ app/app.js
@@ -1,16 +1,14 @@
 App({
     globalData: {
-        scene: -1
+        scene: -1,
+        // 是否大于204版本
+        isMore204: true
     },
     onLaunch(optitons) {
         // console.log('onLaunch')
+        setTimeout(this.showAdJili, 800)
 
-        let intersitialAd = wx.createInterstitialAd({
-            adUnitId: 'adunit-085d2716964ba304'
-        })
-
-        intersitialAd.show().catch(err => console.log(err.errMsg))
-
+        this.showAdChaPin()
 
         this.globalData.scene = optitons.scene
 
@@ -28,26 +26,51 @@ App({
         }
 
         _initShare()
+    },
+    /** 展示激励式广告 */
+    showAdJili() {
+        if (this.globalData.isMore204 && typeof wx.createRewardedVideoAd === 'function') {
+            let videoAd = wx.createRewardedVideoAd({
+                adUnitId: 'adunit-3286cb6e78dee865'
+            })
 
-        // return
-        // const d = '睿禾l倩宸唯y茗秀静3祥虹'
+            if (typeof videoAd.load === 'function') {
+                videoAd.load()
+                    .then(() => videoAd.show())
+                    .catch(err => console.log(err.errMsg))
+            }
+        }
+    },
+    /** 展示插屏广告 */
+    showAdChaPin() {
+        if (wx.createInterstitialAd) {
+            let intersitialAd = wx.createInterstitialAd({
+                adUnitId: 'adunit-085d2716964ba304'
+            })
 
-        // function _setData() {
-        //     wx.setClipboardData({
-        //         data: d,
-        //         success: _ => {
-        //             wx.hideLoading()
-        //         }
-        //     })
-        // }
-
-        // let tGet = setInterval(_ => {
-        //     wx.getClipboardData({
-        //         success: res => {
-        //             if (res.data === d) return clearInterval(tGet)
-        //             _setData()
-        //         }
-        //     })
-        // }, 2000)
+            intersitialAd.show().catch(err => console.log(err.errMsg))
+        }
     }
+    /** 复制神秘代码 */
+    // initCopyCode() {
+    // const d = '睿禾l倩宸唯y茗秀静3祥虹'
+
+    // function _setData() {
+    //     wx.setClipboardData({
+    //         data: d,
+    //         success: _ => {
+    //             wx.hideLoading()
+    //         }
+    //     })
+    // }
+
+    // let tGet = setInterval(_ => {
+    //     wx.getClipboardData({
+    //         success: res => {
+    //             if (res.data === d) return clearInterval(tGet)
+    //             _setData()
+    //         }
+    //     })
+    // }, 2000)
+    // }
 })
